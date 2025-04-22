26fc699c3d2e608a2d6febf146ea7c2a6dd26fa3
mac <mac@heimeis-MacBook-Pro.local>
2025-04-22 19:12:48 -0400

    level 2 complete

---
+++ level_two.py
@@ -0,0 +1,5 @@
+mix_of_stuff = ["0","True","False"]
+playlist_for_coding = ["Just the two of us", "Table for two", "Nostalgia", "Best part", "Holy"]
+playlist_for_coding.append("Never gonna let you go")
+playlist_for_coding.append("Gravity")
+print(playlist_for_coding[5])
